29a46f0ddf405f0accb72510946eff3518da0fce
kk <113894966+kk0x03@users.noreply.github.com>
2022-10-14 15:40:07 +0800

    Add files via upload

---
+++ kk.js
@@ -0,0 +1,9 @@
+const http = require("http")
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+
+    response.end('hello world\n');
+
+}).listen(5656);
+
+console.log('finish');
